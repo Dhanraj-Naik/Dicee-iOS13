aca2d1547c615fed1e23565253ec1a2d0e95f40a
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2020-11-21 23:26:00 +0530

    -added random function on array

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,24 +15,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    var leftDiceNumber = 2
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceOne")
-        diceImageViewOne.alpha = 0.5
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImageViewOne.alpha = 0.5
         
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageViewTwo.alpha = 0.5
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageViewTwo.alpha = 0.5
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageViewOne.alpha = 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+//        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewOne.image = diceArray.randomElement() //it will do exactly same
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
         
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.alpha = 1
+//        leftDiceNumber += Int.random(in: 0...5)
+//        rightDiceNumber -= 1
         
         
         print("Button clicked re")
